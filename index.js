2f563bba43c1f69cf0e729b74f967bb677b102ec
Daniele Corvari <corvari.1883299@studenti.uniroma1.it>
2022-06-03 15:15:30 +0200

    aggiunti commenti

--- index.js
+++ index.js
@@ -23,25 +23,7 @@ const multer = require('multer');			//Abilita il file upload verso il server
 
 const {extractColors} = require("extract-colors")	//Funzione per estrazione di colori dalle foto in locale, senza utilizzare le API di Imagga
 
-
-
-/**************  Passport declarations **************/
-function checkAuthenticated(req, res, next) { //controllo se l'utente è autenticato
-	if (req.isAuthenticated()) {
-		return next()
-	}
-	return res.redirect('/login')
-}
-
-function checkNotAuthenticated(req, res, next) { //controllo se l'utente NON è autenticato
-	if (req.isAuthenticated()) {
-		return res.redirect('/')
-	}
-	return next()
-}
-
-
-/**************  Creazione CouchDb   ************** */
+/************************************************************  CouchDb setup ***********************************************************/
 const couch = new NodeCouchDb({
 	auth: {
 		user: process.env.DB_USER,
@@ -56,7 +38,13 @@ function view(doc) {
 	emit(doc._id, {name: doc.name, user: doc.user, song_number: doc.song_number, songs: doc.songs});
   }
 
+/************************************************************  Passport setup ***********************************************************/
+
+/* passport è stato utilizzato sia per l'autenticazione tramite google e spotify, sia come gestore di sessione */
+
+
 //STRATEGIA PASSPORT SPOTIFY
+
 const spotify_users = new Map();
 const spotify_timers = new Map();
 const spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
@@ -83,17 +71,13 @@ const spotify_scopes = [
 	'user-follow-modify'
 ];//da ottimizzare
 
-/* var spotifyApi = new SpotifyWebApi({
-	clientId: spotify_client_id,
-	clientSecret: spotify_client_secret,
-}); */
 passport.use('spotify',
 	new SpotifyStrategy({
 		clientID: spotify_client_id,
 		clientSecret: spotify_client_secret,
 		callbackURL: '/spotify-login/callback'
 	},
-		async function (accessToken, refreshToken, expiresIn, profile, done) {
+		async function (accessToken, refreshToken, expiresIn, profile, done) {//verify callback per spotify
 			let spotifyApi=  new SpotifyWebApi({
 				clientId: spotify_client_id,
 				clientSecret: spotify_client_secret,
@@ -103,7 +87,7 @@ passport.use('spotify',
 			
 			spotifyApi.setAccessToken(accessToken);
 			spotifyApi.setRefreshToken(refreshToken);
-			let tastes =await spotifyUtils.getUserTaste(spotifyApi)
+			let tastes =await spotifyUtils.getUserTaste(spotifyApi)//ottengo i gusti dell'utente
 
 			//gestisco il refreshtoken automatizzando la richiesta di un nuovo accesstoken
 			let intervalID=	setInterval(async () => {
@@ -119,7 +103,7 @@ passport.use('spotify',
 			else prof_pic = profile.photos[0].value
 
 			
-			spotify_users.set(profile.id,{
+			spotify_users.set(profile.id,{//registro i dati dell'utente pronti per essere deserializzati
 				id: profile.id,
 				name: profile.displayName,
 				prof_pic: prof_pic,
@@ -134,6 +118,34 @@ passport.use('spotify',
 			
 		}
 	)
+)
+
+//STRATEGIA PASSPORT GOOGLE
+
+const google_client_id = process.env.GOOGLE_CLIENT_ID;
+const google_client_secret = process.env.GOOGLE_CLIENT_SECRET;
+const google_scopes=[
+	'https://www.googleapis.com/auth/photoslibrary.readonly', 
+	'https://www.googleapis.com/auth/userinfo.profile'
+]
+const google_users=new Map();
+passport.use('google',
+	new GoogleStrategy({
+		clientID: google_client_id,
+		clientSecret: google_client_secret,
+		callbackURL: '/google-login/callback'
+	},
+		async function (accessToken, refreshToken, profile, cb) {//verify callback per spotify
+			//ottengo gli album dell'utente tramite accessToken
+			
+			let data = await googleUtils.getAlbums(accessToken)
+			google_users.set(profile.id,{
+				id: profile.id,
+				albums: data,
+				accessToken: accessToken
+			})
+			return cb(null, profile)
+		})
 )
 //serializza Utente
 passport.serializeUser(function (user, done) {
@@ -153,36 +165,40 @@ passport.deserializeUser(function (id, done) {
 		//google_users.delete(id)
 	}
 })
-//STRATEGIA PASSPORT GOOGLE
-const google_client_id = process.env.GOOGLE_CLIENT_ID;
-const google_client_secret = process.env.GOOGLE_CLIENT_SECRET;
-const google_scopes=[
-	'https://www.googleapis.com/auth/photoslibrary.readonly', 
-	'https://www.googleapis.com/auth/userinfo.profile'
-]
-const google_users=new Map();
-passport.use('google',
-	new GoogleStrategy({
-		clientID: google_client_id,
-		clientSecret: google_client_secret,
-		callbackURL: '/google-login/callback'
+function checkAuthenticated(req, res, next) { //controllo se l'utente è autenticato
+	if (req.isAuthenticated()) {
+		return next()
+	}
+	return res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) { //controllo se l'utente NON è autenticato
+	if (req.isAuthenticated()) {
+		return res.redirect('/')
+	}
+	return next()
+}
+/**************************************************************  Multer Setup ************************************************************************/
+
+/* multer è stato utilizzato per gestire l'input tramite file della funzionalità principale */
+
+const fileStorageEngine = multer.diskStorage({
+	destination: (req, file, callback) => {
+	  callback(null, './public/images')
 	},
-		async function (accessToken, refreshToken, profile, cb) {
-			//ottengo gli album dell'utente tramite accessToken
-			
-			let data = await googleUtils.getAlbums(accessToken)
-			google_users.set(profile.id,{
-				id: profile.id,
-				albums: data,
-				accessToken: accessToken
-			})
-			return cb(null, profile)
-		})
-)
+	filename: (req, file, callback) => {
+	  callback(null, Date.now()  + '--' + file.originalname);
+	}
+});
+
+const upload = multer({ storage : fileStorageEngine })
+
+
+
 
 
 const app = express();
-/**************  **************/
+/**************  Mounting vari **************/
 
 app.set('view-engine', 'ejs');
 app.use(express.urlencoded({ extended: false }))
@@ -199,24 +215,12 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+/********************************************** Routing section of the server setup *********************************************************/
 
+/* sono state utilizzare le due funzioni middleware checkAuthenticated e checkNotAuthenticated prima di 
+gestire le richieste per verificare che l'utente sia loggato e che quindi abbia effettivamente accesso alla risorsa richiesta */
 
-
-/**************  Aux function multer **************/
-
-const fileStorageEngine = multer.diskStorage({
-	destination: (req, file, callback) => {
-	  callback(null, './public/images')
-	},
-	filename: (req, file, callback) => {
-	  callback(null, Date.now()  + '--' + file.originalname);
-	}
-});
-
-const upload = multer({ storage : fileStorageEngine })
-
-
-/**************  Gestione della home **************/
+/**************************  Gestione della home ****************************/
 
 app.get('/', /* checkNotAuthenticated, */(req, res) => {
 	let p2sUser = null
@@ -226,12 +230,10 @@ app.get('/', /* checkNotAuthenticated, */(req, res) => {
 			user_image: req.session.user.prof_pic
 		}
 	}
-	res.render('./pages/landing_page.ejs', { p2sUser: p2sUser })
+	res.render('./pages/landing_page.ejs', { p2sUser: p2sUser })/*render della pagina landing_page.ejs*/
 
 })
-
-//passport.authenticate per autenticare l'utente all'interno del sito con successivo redirect in caso di fallimento o di successo
-
+/**************  Gestione di login e delle varie autenticazioni **************/
 app.get('/login', checkNotAuthenticated, (req, res) => {
 	let p2sUser=null
 	if(req.session.user!==undefined){
@@ -253,47 +255,47 @@ app.post('/logout', checkAuthenticated, (req, res) => {
 	res.redirect('/')
 })
 
-
-/************** Listening section of the server setup **************/
-
-//questo setup fa solo da ponte, al click sul pulsante "log me in with spotify" il browser dell'utente effettua una get a /spotify-login...
+//Passport.authenticate per ottenere l'autorizzazione nell'utilizzare i dati dell'utente, ottengo access code
 app.get('/spotify-login', checkNotAuthenticated, passport.authenticate('spotify', { scope: spotify_scopes }));
 
-
+//Passport.authenticate per autenticare, on callback viene richiamata la verify callback di passport definita precedentemente
 app.get('/spotify-login/callback', checkNotAuthenticated,passport.authenticate('spotify', {
 	successRedirect: '/spotify-login/callback/return',
 	failureRedirect: '/'
 }))
 app.get('/spotify-login/callback/return',checkAuthenticated,function(req,res){
 	if(req.session.user===undefined){
-		req.session.user=req.user
+		req.session.user=req.user //monto nella sessione i dati utili dell'utente deserializzato rendendoli accessibili ovunque lui vada
 	}
 	res.redirect('/')
 })
 
 
-//anche se non strettamente necessario per comprensibilità ho utilizzato lo stesso "flow" utilizzato per spotify
-//Passport.authenticate per ottenere l'autorizzazione nell'utilizzare lo scope photoslibrary.readonly dell'utente
-app.get('/google-login', checkAuthenticated, passport.authenticate('google', { scope: google_scopes })); //function(req, res){  //chiamato dal file input#.ejs
+//per google anche se non strettamente necessario è stato utilizzato lo stesso "flow" utilizzato per spotify 
 
 
-//passport.authenticate per autenticare l'utente all'interno del sito con successivo redirect in caso di fallimento o successo
+//Passport.authenticate per ottenere l'autorizzazione nell'utilizzare lo scope photoslibrary.readonly dell'utente, ottengo access code
+app.get('/google-login', checkAuthenticated, passport.authenticate('google', { scope: google_scopes }));
+
+
+//Passport.authenticate per autenticare, on callback viene richiamata la verify callback di passport definita precedentemente
 app.get('/google-login/callback', checkAuthenticated, passport.authenticate('google', {
 	successRedirect: '/google-login/callback/return',
 	failureRedirect: '/input'
 }));
 app.get('/google-login/callback/return',checkAuthenticated ,function(req,res){
 	req.session.user.albums=req.user.albums
-	req.session.user.accessTokenGoogle=req.user.accessToken
+	req.session.user.accessTokenGoogle=req.user.accessToken //aggiungo alla sessione i dati utili dell'utente google deserializzato 
 	res.redirect('/input')
 })
 
+/************** Gestione della funzionalità principali Photo to Song **************/
 
-/************** Gestione dell'input **************/
-app.get('/input', checkAuthenticated, function (req, res) { // input prima del login con google 
+/* Gestione dell'input */
+app.get('/input', checkAuthenticated, function (req, res) { 
 	res.render('./pages/input.ejs', 
 	{ 
-		albums: req.session.user.albums,
+		albums: req.session.user.albums,//passo in rendering della pagina di input gli albums di google se presenti
 		logged: req.session.user.accessTokenGoogle!='', 
 		p2sUser: {
 			username: req.session.user.name,
@@ -301,7 +303,7 @@ app.get('/input', checkAuthenticated, function (req, res) { // input prima del l
 		} 
 	})
 });
-app.post('/checkUrl', async function (req, res) { // input prima del login con google 
+app.post('/checkUrl', async function (req, res) { // verifica preventiva che l'url sia accessibili dal nostro server (ad esempio non abbiamo accesso a foto di wikipedia)
 	try {
 		let colors = await extractColors(req.body.url)
 	} catch (e) {
@@ -313,30 +315,35 @@ app.post('/checkUrl', async function (req, res) { // input prima del login con g
 });
 
 
-/************** Gestione del risultato **************/
+/* Gestione del risultato */
 
 const userData = new Map();
 
 app.post('/result',upload.array("images", 50), checkAuthenticated, function (req, res) {
-	userData.set(req.session.user.id,{
-		photos: Array(),
-		names: Array(),
-		songsDB: Array(),
-		songsChosen: Array()
+	userData.set(req.session.user.id,{// ogni volta che un utente effettua una post a questa risorsa viene creata una struttura che avrà come chiave di accesso il suo id
+		photos: Array(),//foto in input
+		names: Array(),//nomi delle foto in input
+		songsDB: Array(),// struttura da inserire in un documento del database se l'utente decide di salvare la playlist
+		songsChosen: Array()// struttura utile solo per evitare la ripetizione di una canzone in una richiesta
 	})
 	p2sUser={
 		username: req.session.user.name,
 		user_image: req.session.user.prof_pic
 	}
-	
-	if (req.files) {
+
+	/* in base al tipo di input preparo le strutture sopra elencate prima di renderizzare la pagina dei risultati.
+	In particolare nella struttura photos verranno salvate le foto della richiesta dell'utente. Da questa struttura ad ogni chiamata ajax a getSong verrà prelevata una foto e analizzata. 
+	Inoltre vengono preparati i nomi per le foto per il database e per la funzionalità plist history e vengono preparati url o path per il carosello della pagina result.ejs*/
+
+
+	if (req.files) {//caso files
 		userData.get(req.session.user.id).photos=req.files;
 		let photos=userData.get(req.session.user.id).photos
 		let names=userData.get(req.session.user.id).names
 		if(photos.length!=0){
 			let urls=Array();
 			
-			for (let index = 0; index < userData.get(req.session.user.id).photos.length; index++) {
+			for (let index = 0; index < userData.get(req.session.user.id).photos.length; index++) {//prendo il nome del file e preparo il path per ogni file
 				names.push(photos[index].path.substring(photos[index].path.indexOf("-") + 2))
 				urls.push(photos[index].path.substring(6));
 			}
@@ -345,11 +352,11 @@ app.post('/result',upload.array("images", 50), checkAuthenticated, function (req
 		}
 		else res.redirect('/input');
 	}
-	else if (req.body.urls) {
+	else if (req.body.urls) {//caso url
 		let photos=userData.get(req.session.user.id).photos
 		let names=userData.get(req.session.user.id).names
 		try{
-			req.body.urls.forEach(element=>{
+			req.body.urls.forEach(element=>{//utilizzo l'url stesso sia come nome sia come reference alla foto
 				photos.push(element)
 				names.push(element)
 			})
@@ -361,7 +368,7 @@ app.post('/result',upload.array("images", 50), checkAuthenticated, function (req
 			res.render('./pages/result.ejs', { urls: photos,num: photos.length, p2sUser: p2sUser })
 		}
 	}
-	else if (req.body.album) {//finito
+	else if (req.body.album) {//caso album di google
 		i = req.body.album
 		let album=req.session.user.albums[i]
 		
@@ -369,9 +376,9 @@ app.post('/result',upload.array("images", 50), checkAuthenticated, function (req
 			.then(data => {
 				let photos=userData.get(req.session.user.id).photos
 				let names=userData.get(req.session.user.id).names
-				data.forEach(element => { //concateno <titolo dell'album>.<nome della foto> come identificatore delle foto per il DB 
+				data.forEach(element => { //concateno <titolo dell'album>.<nome della foto> come nome delle foto per il DB 
 					names.push( album.title + "." + element.filename );
-					photos.push(element.baseUrl)
+					photos.push(element.baseUrl)//utilizzo l'url della foto di google foto come reference alla foto
 				});
 
 
@@ -381,7 +388,7 @@ app.post('/result',upload.array("images", 50), checkAuthenticated, function (req
 	else res.redirect('/input');
 	
 })
-
+/* Gestione delle richieste di save delle playlist*/
 app.post('/playlist', checkAuthenticated, function (req, res) {
 
 	let songsDB = Array.from(userData.get(req.session.user.id).songsDB)
@@ -416,7 +423,7 @@ app.post('/playlist', checkAuthenticated, function (req, res) {
 
 
 
-	couch.uniqid().then((ids) => {
+	couch.uniqid().then((ids) => {// aggiungo la playlist al DB
         const id = ids[0]
 		couch.insert(dbName, {
 			_id: id,
@@ -429,28 +436,28 @@ app.post('/playlist', checkAuthenticated, function (req, res) {
 	})
 	res.redirect('/')
 })
-
+/* Gestione delle richieste ajax lanciate dallo script getSong del file main_result_scripts.js*/
 app.get('/getSong',checkAuthenticated,async function (req, res) {
 	let data	
 	try{
-		let photo = userData.get(req.session.user.id).photos.pop();
+		let photo = userData.get(req.session.user.id).photos.pop();//prendo una foto
 		let imgName = userData.get(req.session.user.id).names.pop();
 		if (photo == null) data=null
 		else{
 			let song
-			if(photo.path !== undefined){
-				song = await spotifyUtils.getSongFromColors(extractColors(photo.path), req.session.user.tastes, userData.get(req.session.user.id).songsChosen)	
+			if(photo.path !== undefined){//verifico se in input ho un file o un url
+				song = await spotifyUtils.getSongFromColors(extractColors(photo.path), req.session.user.tastes, userData.get(req.session.user.id).songsChosen)//scelgo la canzone
 				userData.get(req.session.user.id).songsChosen.push(song)				
 			}
 			else{
-				song = await spotifyUtils.getSongFromColors(extractColors(photo), req.session.user.tastes, userData.get(req.session.user.id).songsChosen)		
+				song = await spotifyUtils.getSongFromColors(extractColors(photo), req.session.user.tastes, userData.get(req.session.user.id).songsChosen)//scelgo la canzone	
 				userData.get(req.session.user.id).songsChosen.push(song)
 			}
 		
 			userData.get(req.session.user.id).songsDB.push({
 				song: song,
 				photo: imgName
-			})
+			})//inserisco nella struttura relativa all'utente
 			data=song
 		}
 	}catch(e){
@@ -458,7 +465,7 @@ app.get('/getSong',checkAuthenticated,async function (req, res) {
 		data='error'
 	}
 	if (data=='error') res.redirect('/')
-	else if (data) res.send(data)
+	else if (data) res.send(data)/* se le foto non erano finite mando la canzone scelta */
 	else res.send('end')
 })
 
@@ -492,10 +499,6 @@ app.post('/plist-analyzer', (req, res) => {
 	})
 })
 
-app.listen(8888, () => {
-	console.log('Server listening on http://localhost:8888/');
-});
-
 /************** Funzionalità: Playlist History ******************* */
 
 app.get('/plist-history', checkAuthenticated, (req, res) => {
@@ -515,3 +518,7 @@ app.get('/plist-history', checkAuthenticated, (req, res) => {
         }
     );
 })
+//metto il server in ascolto
+app.listen(8888, () => {
+	console.log('Server listening on http://localhost:8888/');
+});
